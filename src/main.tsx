7053b8a38b3b5e9570c73894e8aa4c343ce287e8
Sebastian-Olah <olahseb68@gmail.com>
2026-03-17 14:37:00 +0000

    added AppProvider to main.tsx to wrap app with global context

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AppProvider } from './context/AppContext.tsx'
 
+// AppProvider wraps the entire app so global state is accessible everywhere
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </StrictMode>,
 )
