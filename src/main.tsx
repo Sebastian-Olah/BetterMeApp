4f6b3024e1ddc338d2b7dd30f2b14c39d87572c1
Sebastian-Olah <olahseb68@gmail.com>
2026-04-01 14:07:00 +0000

    FINAL

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { AppProvider } from './context/AppContext.tsx'
+import { AppProvider } from './context/AppConext.tsx'
 
 // AppProvider wraps the entire app so global state is accessible everywhere
 createRoot(document.getElementById('root')!).render(
